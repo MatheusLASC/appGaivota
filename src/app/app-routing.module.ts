1bf74329c16cd3bd5d127a6fe25827c821e6ae63
Matheus Lima de Almeida <matheuslima.tipi@gmail.com>
2021-03-28 16:59:14 -0300

    Estruturando as Páginas e Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'artist',
+    loadChildren: () => import('./pages/artist/artist.module').then( m => m.ArtistPageModule)
+  },
+  {
+    path: 'hq',
+    loadChildren: () => import('./pages/hq/hq.module').then( m => m.HqPageModule)
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./pages/list/list.module').then( m => m.ListPageModule)
+  },
+
+
+
 ];
 
 @NgModule({
