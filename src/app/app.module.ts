3c4f69788835a847ab6c85ccc552175697619d31
Matheus Lima de Almeida <matheuslima.tipi@gmail.com>
2021-05-01 19:10:42 -0300

    Nova Estrutura para Cloud Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import {AngularFireModule} from '@angular/fire';
-import { environment } from 'src/environments/environment';
 import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule],
+    AngularFireAuthModule,
+    AngularFirestoreModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
