74e5f40a5deb6d89eb00cb8208b796d1fbfcd3c7
Matheus Lima de Almeida <matheuslima.tipi@gmail.com>
2021-05-01 19:10:42 -0300

    Nova Estrutura para Cloud Firebase

---
+++ src/app/interfaces/artist.ts
@@ -0,0 +1,16 @@
+export interface Artist {
+  id?: string;
+  name?: string;
+  nameartist?: string;
+  CPF?: string;
+  cidade?: string;
+  state?: string;
+  biography?: string;
+  picture?: string;
+  facebook?: string;
+  instagram?: string;
+  twitter?: string;
+  site?: string;
+  portfolio?: string;
+  userid?: string;
+}
---
+++ src/app/services/artists.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Artist } from '../interfaces/artist';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArtistsService {
+  private artistsCollection: AngularFirestoreCollection<Artist>;
+
+  constructor(private afs: AngularFirestore) {
+    this.artistsCollection = this.afs.collection<Artist>('Artists');
+   }
+
+   getArtists(){
+    return this.artistsCollection.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+
+          return { id, ...data };
+        });
+      })
+    );
+  }
+
+  addProduct(artist: Artist) {
+    return this.artistsCollection.add(artist);
+  }
+
+  getProduct(id: string) {
+    return this.artistsCollection.doc<Artist>(id).valueChanges();
+  }
+
+   }
+
